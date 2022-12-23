4fb13a7732993a75b3fe6cfc20f61ae0d6742ca8
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2022-12-24 00:34:49 +0530

    added reset function and basic sprites

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,29 @@ const RAM_SIZE: usize = 4096;
 pub const SCREEN_WIDTH: usize = 64;
 pub const SCREEN_HEIGHT: usize = 32;
 const START_ADDR: u16 = 0x200;
-
+const FONTSET_SIZE: usize = 80;
+const FONTSET: [u8; FONTSET_SIZE] = [
+0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+0x20, 0x60, 0x20, 0x20, 0x70, // 1
+0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+];
 
 pub struct Emulator {
     pc: u16,
-    sp: u8,
+    sp: u16,
     ram: [u8; RAM_SIZE],
     screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
     v_registers: [u8; 16],
@@ -19,7 +37,7 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new() -> Self {
-        Self {
+        let mut new_emu =  Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -30,6 +48,32 @@ impl Emulator {
             sound_timer: 0,
             delay_timer: 0,
             keys: [false; 16],
-        }
+        };
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        new_emu
+
+    }
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize]
+    }
+    pub fn reset(&mut self) {
+            self.pc= START_ADDR;
+            self.ram= [0; RAM_SIZE];
+            self.screen= [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+            self.v_registers= [0; 16];
+            self.i_register= 0;
+            self.sp= 0;
+            self.stack= [0; 16];
+            self.sound_timer= 0;
+            self.delay_timer= 0;
+            self.keys= [false; 16];
+            self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
     }
 }
