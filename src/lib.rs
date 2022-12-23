dab0b0745521f60f3a081942670346b1fa662ad2
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2022-12-24 00:47:12 +0530

    added a tcik function to fetch opcodes

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,22 @@ pub const SCREEN_HEIGHT: usize = 32;
 const START_ADDR: u16 = 0x200;
 const FONTSET_SIZE: usize = 80;
 const FONTSET: [u8; FONTSET_SIZE] = [
-0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-0x20, 0x60, 0x20, 0x20, 0x70, // 1
-0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
 
 pub struct Emulator {
@@ -37,7 +37,7 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new() -> Self {
-        let mut new_emu =  Self {
+        let mut new_emu = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -51,7 +51,6 @@ impl Emulator {
         };
         new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
         new_emu
-
     }
     fn push(&mut self, val: u16) {
         self.stack[self.sp as usize] = val;
@@ -63,17 +62,40 @@ impl Emulator {
         self.stack[self.sp as usize]
     }
     pub fn reset(&mut self) {
-            self.pc= START_ADDR;
-            self.ram= [0; RAM_SIZE];
-            self.screen= [false; SCREEN_WIDTH * SCREEN_HEIGHT];
-            self.v_registers= [0; 16];
-            self.i_register= 0;
-            self.sp= 0;
-            self.stack= [0; 16];
-            self.sound_timer= 0;
-            self.delay_timer= 0;
-            self.keys= [false; 16];
-            self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
-
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_registers = [0; 16];
+        self.i_register = 0;
+        self.sp = 0;
+        self.stack = [0; 16];
+        self.sound_timer = 0;
+        self.delay_timer = 0;
+        self.keys = [false; 16];
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+    pub fn tick(&mut self) {
+        // Fetch
+        let op = self.fetch();
+        // Decode
+        // Execute
+    }
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
+    pub fn tick_timers(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+        if self.sound_timer > 0 {
+            if self.sound_timer == 1 {
+                // BEEP
+            }
+            self.st -= 1;
+        }
     }
 }
