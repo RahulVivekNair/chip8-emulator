22488617cba7595118a528917648347c082f18c5
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2022-12-24 15:21:26 +0530

    added keyboard interactions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chip8_emulator::*;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
@@ -7,7 +8,6 @@ use sdl2::video::Window;
 use std::env;
 use std::fs::File;
 use std::io::Read;
-
 const SCALE: u32 = 15;
 const TICKS_PER_FRAME: usize = 10;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
@@ -46,14 +46,29 @@ fn main() {
     'gameloop: loop {
         for evt in event_pump.poll_iter() {
             match evt {
-                Event::Quit { .. } => {
+                Event::Quit { .. } | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => {
                     break 'gameloop;
                 }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                }
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
+                }
+
                 _ => (),
             }
         }
         //for _ in 0..TICKS_PER_FRAME {
-            chip8.tick();
+        chip8.tick();
         //}
         chip8.tick_timers();
         draw_screen(&chip8, &mut canvas)
@@ -77,4 +92,25 @@ fn draw_screen(emu: &Emulator, canvas: &mut Canvas<Window>) {
         }
     }
     canvas.present();
+}
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
 }
