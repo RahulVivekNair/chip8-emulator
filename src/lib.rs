6b7683e0f7cb5a75cfb7125f10a5dfa2cff8c99d
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2022-12-24 13:06:20 +0530

    added more opcodes

--- src/lib.rs
+++ src/lib.rs
@@ -96,130 +96,220 @@ impl Emulator {
             //Return from subroutine
             (0, 0, 0xE, 0xE) => {
                 self.pc = self.pop();
-            },
+            }
             //Jump to address
             (1, _, _, _) => {
                 self.pc = op & 0x0FFF;
-            },
+            }
             //Call subroutine
             (2, _, _, _) => {
                 self.push(self.pc);
                 self.pc = op & 0x0FFF;
-            },
+            }
             //skip next instruction
-            (3,_,_,_) => {
+            (3, _, _, _) => {
                 let reg = h_digit2;
                 let val = op & 0x00FF;
                 if self.v_registers[reg as usize] == val as u8 {
                     self.pc += 2;
-                } 
-            },
+                }
+            }
             //skip next instruction if not equal
-            (4,_,_,_) => {
+            (4, _, _, _) => {
                 let reg = h_digit2;
                 let val = op & 0x00FF;
                 if self.v_registers[reg as usize] != val as u8 {
                     self.pc += 2;
-                } 
-            },
+                }
+            }
             //skip next instruction if reg values are equal
-            (5,_,_,_) => {
+            (5, _, _, _) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
                 if self.v_registers[reg1] == self.v_registers[reg2] {
                     self.pc += 2;
-                } 
-            },
+                }
+            }
             //set register to value
-            (6,_,_,_) => {
+            (6, _, _, _) => {
                 let reg = h_digit2 as usize;
                 let val = op & 0x00FF;
                 self.v_registers[reg] = val as u8;
-            },
+            }
             //add value to register
-            (7,_,_,_) => {
+            (7, _, _, _) => {
                 let reg = h_digit2 as usize;
                 let val = op & 0x00FF;
                 //wrapping add to avoid overflowss
                 self.v_registers[reg] = self.v_registers[reg].wrapping_add(val as u8);
-            },
+            }
             //set register to value of another register
-            (8,_,_,0) => {
+            (8, _, _, 0) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
                 self.v_registers[reg1] = self.v_registers[reg2];
-            },
+            }
             //bitwise or
-            (8,_,_,1) => {
+            (8, _, _, 1) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
                 self.v_registers[reg1] |= self.v_registers[reg2];
-            },
+            }
             //bitwise and
-            (8,_,_,2) => {
+            (8, _, _, 2) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
                 self.v_registers[reg1] &= self.v_registers[reg2];
-            },
+            }
             //bitwise xor
-            (8,_,_,3) => {
+            (8, _, _, 3) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
                 self.v_registers[reg1] ^= self.v_registers[reg2];
-            },
+            }
             //add register to another register
-            (8,_,_,4) => {
+            (8, _, _, 4) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
-                let (val, overflow) = self.v_registers[reg1].overflowing_add(self.v_registers[reg2]);
+                let (val, overflow) =
+                    self.v_registers[reg1].overflowing_add(self.v_registers[reg2]);
                 self.v_registers[reg1] = val;
-                self.v_registers[0xF] = if overflow {1} else {0};
-            },
+                self.v_registers[0xF] = if overflow { 1 } else { 0 };
+            }
             //subtract register from another register
-            (8,_,_,5) => {
+            (8, _, _, 5) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
-                let (val, overflow) = self.v_registers[reg1].overflowing_sub(self.v_registers[reg2]);
+                let (val, overflow) =
+                    self.v_registers[reg1].overflowing_sub(self.v_registers[reg2]);
                 self.v_registers[reg1] = val;
-                self.v_registers[0xF] = if overflow {0} else {1};
-            },
+                self.v_registers[0xF] = if overflow { 0 } else { 1 };
+            }
             //shift right and store dropped bit in VF
-            (8,_,_,6) => {
+            (8, _, _, 6) => {
                 let reg1 = h_digit2 as usize;
                 self.v_registers[0xF] = self.v_registers[reg1] & 0x1;
                 self.v_registers[reg1] >>= 1;
-            },
+            }
             //subtract register from another register
-            (8,_,_,7) => {
+            (8, _, _, 7) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
-                let (val, overflow) = self.v_registers[reg2].overflowing_sub(self.v_registers[reg1]);
+                let (val, overflow) =
+                    self.v_registers[reg2].overflowing_sub(self.v_registers[reg1]);
                 self.v_registers[reg1] = val;
-                self.v_registers[0xF] = if overflow {0} else {1};
-            },
+                self.v_registers[0xF] = if overflow { 0 } else { 1 };
+            }
             //shift left and store dropped bit in VF
-            (8,_,_,0xE) => {
+            (8, _, _, 0xE) => {
                 let reg1 = h_digit2 as usize;
                 self.v_registers[0xF] = self.v_registers[reg1] >> 7;
                 self.v_registers[reg1] <<= 1;
-            },
+            }
             //skip next instruction if reg values are not equal
-            (9,_,_,0) => {
+            (9, _, _, 0) => {
                 let reg1 = h_digit2 as usize;
                 let reg2 = h_digit3 as usize;
                 if self.v_registers[reg1] != self.v_registers[reg2] {
                     self.pc += 2;
-                } 
-            },
+                }
+            }
             //set I to value
-            (0xA,_,_,_) => {
+            (0xA, _, _, _) => {
                 let val = op & 0x0FFF;
                 self.i_register = val;
-            },
+            }
             //jump to address + value of register 0
-            (0xB,_,_,_) => {
+            (0xB, _, _, _) => {
                 let val = op & 0x0FFF;
                 self.pc = val + self.v_registers[0] as u16;
+            }
+            //set register to random number and value
+            (0xC, _, _, _) => {
+                let reg = h_digit2 as usize;
+                let val = op & 0x00FF;
+                let rand = fastrand::u8(..);
+                self.v_registers[reg] = rand & val as u8;
+            }
+            //draw sprite
+            (0xD, _, _, _) => {
+                let x_coord = self.v_registers[h_digit2 as usize] as usize;
+                let y_coord = self.v_registers[h_digit3 as usize] as usize;
+                let height = h_digit4 as usize;
+                let mut flipped = false;
+                for y_line in 0..height {
+                    let pixels = self.ram[(self.i_register + y_line as u16) as usize];
+                    for x_line in 0..8 as usize {
+                        if (pixels & (0x80 >> x_line)) != 0 {
+                            let x = (x_coord + x_line) as usize % SCREEN_WIDTH;
+                            let y = (y_coord + y_line) as usize % SCREEN_HEIGHT;
+                            let idx = x + SCREEN_WIDTH * y;
+                            // Check if we're about to flip the pixel and set
+                            flipped |= self.screen[idx];
+                            self.screen[idx] ^= true;
+                        }
+                    }
+                }
+                if flipped {
+                    self.v_registers[0xF] = 1;
+                } else {
+                    self.v_registers[0xF] = 0;
+                }
+            }
+            //skip next instruction if key in register is pressed
+            (0xE, _, 9, 0xE) => {
+                let reg = h_digit2 as usize;
+                let key = self.v_registers[reg];
+                if self.keys[key as usize] {
+                    self.pc += 2;
+                }
+            }
+            //skip next instruction if key in register is not pressed
+            (0xE, _, 0xA, 1) => {
+                let reg = h_digit2 as usize;
+                let key = self.v_registers[reg];
+                if !self.keys[key as usize] {
+                    self.pc += 2;
+                }
+            }
+            //set register to delay timer
+            (0xF, _, 0, 7) => {
+                let reg = h_digit2 as usize;
+                self.v_registers[reg] = self.delay_timer;
+            }
+            //wait for keypress and store in register
+            (0xF, _, 0, 0xA) => {
+                let reg = h_digit2 as usize;
+                let mut key_pressed = false;
+                for i in 0..self.keys.len() {
+                    if self.keys[i] {
+                        self.v_registers[reg] = i as u8;
+                        key_pressed = true;
+                    }
+                }
+                if !key_pressed {
+                    self.pc -= 2;
+                }
+            },
+            //set delay timer to register
+            (0xF, _, 1, 5) => {
+                let reg = h_digit2 as usize;
+                self.delay_timer = self.v_registers[reg];
+            },
+            //set sound timer to register
+            (0xF, _, 1, 8) => {
+                let reg = h_digit2 as usize;
+                self.sound_timer = self.v_registers[reg];
+            },
+            //add register to I
+            (0xF, _, 1, 0xE) => {
+                let reg = h_digit2 as usize;
+                self.i_register = self.i_register.wrapping_add(self.v_registers[reg] as u16);
+            },
+            //set I to location of sprite for digit in register
+            (0xF, _, 2, 9) => {
+                let reg = h_digit2 as usize;
+                self.i_register = (self.v_registers[reg] * 5) as u16;
             },
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {:X}", op),
         }
